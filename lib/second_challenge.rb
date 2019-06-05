da3b6dd984fd7feb7915c078010dfd6fe105a103
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-05 15:34:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,13 @@
 def second_challenge
+  nuarray = []
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
    vegetable: ["carrots", "broccoli", "cucumbers"],
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
+nuarray = groceries.values
+nuarray.flatten
   groceries.values#code your solution here!
 
 
